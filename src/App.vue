058af6598fae4c06a75d2e1a1db3ba5e4b4dbd93
iWhited <franck.dupont.w@gmail.com>
2021-03-31 11:38:18 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Staking hello="coucou" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Staking from './components/Staking.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Staking
   }
 }
 </script>
